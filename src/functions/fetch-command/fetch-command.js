9495e257fd121897c350fc2249724a5253c6fc03
Aaron Turner <aaron@aaronthedev.com>
2019-10-03 14:54:12 -0700

    Restructured how modules are laid out

---
+++ src/functions/fetch-command/fetch-command.js
@@ -0,0 +1,58 @@
+// Function to be set to our fetchCommand in our WasmTerminal Config
+import { fetchCommandFromWAPM } from '@wasmer/wasm-terminal';
+import wasmInit, { lowerI64Imports } from "../../assets/wasm-transformer";
+
+const callbackCommands = {
+  callback: async (args, stdin) => {
+    return `Callback Command Working! Args: ${args}, stdin: ${stdin}`;
+  }
+};
+
+const customWasmModuleUrls = {
+  qjs: "assets/wasm-modules/qjs.wasm",
+  duk: "assets/wasm-modules/duk.wasm"
+};
+
+const commandBinaryCache = {};
+let didInitWasmTransformer = false;
+
+const fetchCommand = async (commandName) => {
+
+  // Check the Cache
+  if (commandBinaryCache[commandName]) {
+    return commandBinaryCache[commandName];
+  }
+
+  const callbackCommand = callbackCommands[commandName];
+  if (callbackCommand) {
+    return callbackCommand;
+  }
+
+  // Look for binary commands
+  let wasmBinary = undefined;
+  
+  const commandUrl = customWasmModuleUrls[commandName];
+
+  if (commandUrl) {
+    const fetched = await fetch(commandUrl);
+    const buffer = await fetched.arrayBuffer();
+    wasmBinary = new Uint8Array(buffer);
+  } else {
+    wasmBinary = await fetchCommandFromWAPM(commandName);
+  }
+
+  if (!didInitWasmTransformer) {
+    await wasmInit('/assets/wasm-transformer/wasm_transformer_bg.wasm');
+    didInitWasmTransformer = true;
+  }
+
+  const loweredBinary = lowerI64Imports(wasmBinary);
+
+  // Cache the result
+  commandBinaryCache[commandName] = loweredBinary;
+
+  // Return the binary
+  return loweredBinary;
+};
+
+export default fetchCommand;
--- src/index.js
+++ src/index.js
@@ -1,38 +1,18 @@
 import '../node_modules/normalize.css/normalize.css';
-import '../node_modules/@wasmer/wasm-terminal/dist/xterm/xterm.css';
 import './style';
 import { Component } from 'preact';
-import WasmTerminal, { fetchCommandFromWAPM } from '@wasmer/wasm-terminal';
-import wasmInit, { lowerI64Imports } from "./assets/wasm-transformer";
+import WasmTerminal from '@wasmer/wasm-terminal';
+
+import fetchCommand from './functions/fetch-command/fetch-command';
 
 export default class App extends Component {
 
   constructor() {
     super();
 
-    // Let's write handler for the fetchCommand property of the WasmTerminal Config.
-    const fetchCommandHandler = async commandName => {
-      // Let's return a "CallbackCommand" if our command matches a special name
-      if (commandName === "callback-command") {
-        const callbackCommand = async (args, stdin) => {
-          return `Callback Command Working! Args: ${args}, stdin: ${stdin}`;
-        };
-        return callbackCommand;
-      }
-
-      // Let's fetch a wasm Binary from WAPM for the command name.
-      const wasmBinary = await fetchCommandFromWAPM(commandName);
-
-      // Initialize the Wasm Transformer, and use it to lower
-      // i64 imports from Wasi Modules, so that most Wasi modules
-      // Can run in a Javascript context.
-      await wasmInit('./assets/wasm-transformer/wasm_transformer_bg.wasm');
-      return lowerI64Imports(wasmBinary);
-    };
-
     const wasmTerminal = new WasmTerminal({
       processWorkerUrl: "./assets/wasm-terminal/process.worker.js",
-      fetchCommand: fetchCommandHandler
+      fetchCommand: fetchCommand
     });
 
     this.resizing = false;

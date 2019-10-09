19df6238b2a63c459a503ceefe84ff9a19eeb8ab
Syrus <me@syrusakbary.com>
2019-10-08 19:12:20 -0700

    Added Google analytics

--- src/functions/fetch-command.js
+++ src/functions/fetch-command.js
@@ -7,7 +7,14 @@ let didInitWasmTransformer = false;
 export const wapm = new WAPM(commandBinaryCache);
 
 const fetchCommand = async (commandName) => {
-
+  if (window.gtag) {
+    window.gtag('event', 'run command', {
+      // 'event_category': '',
+      'event_label': commandName,
+      // 'value': '<here the command args and environment>'
+    });
+  }
+  
   // Check the Cache
   if (wapm.isCommandCached(commandName) && commandBinaryCache[commandName]) {
     return commandBinaryCache[commandName];

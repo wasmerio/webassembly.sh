a8c61f707b694012314a5984976db7057001d260
Syrus Akbary <me@syrusakbary.com>
2025-12-01 12:12:09 -0800

    Removed old files

--- src/util/ansi.js
+++
@@ -1,5 +0,0 @@
-export const reset = "\x1B[0m";
-export const magenta = `${reset}\x1B[34;1m`;
-export const bold = `${reset}\x1B[1m`;
-export const underline = `${reset}\x1B[4m`;
-export const boldAndUnderline = `${reset}\x1B[1m\x1B[4m`;
